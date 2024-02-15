33bde876bac89f4d1ae56c199048df84806addb8
mjkweon17 <mjkweon17@korea.ac.kr>
2024-02-16 00:10:20 +0900

    feat: CORS 추가

--- src/main.py
+++ src/main.py
@@ -19,7 +19,8 @@ origins = [
     "http://localhost:8000",
     "http://localhost:8080",
     "http://127.0.0.1:8000/",
-    "https://life-book-tau.vercel.app/"
+    "https://life-book-tau.vercel.app",
+    "https://life-book-tau.vercel.app/",
 ]
 
 app.add_middleware(
