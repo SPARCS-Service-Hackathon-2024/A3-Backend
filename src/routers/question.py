96f8c79c3624ca3f000181522704970a40c7349f
r4bb1t <r4bb1t.github@gmail.com>
2024-02-15 20:09:55 +0900

    feat: update submit_answer function

--- src/routers/question.py
+++ src/routers/question.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 from database import get_db
-from models import LQuestions, LAnswers
+from models import LQuestions, LAnswers, LUsers
 from pydantic import BaseModel
 from routers.auth import get_current_user
 
@@ -19,18 +19,6 @@ async def get_question(id: int, db: Session = Depends(get_db)):
     return {'id': question.question_id, 'content': question.content, 
             'is_answerable': question.is_answerable, 'next_question_id': question.next_question_id}
 
-
-@router.get("/last",
-            summary="마지막으로 답변한 질문의 id 가져오기")
-async def get_last_question(user=Depends(get_current_user), db: Session = Depends(get_db)):
-
-    question = db.query(LQuestions).filter(LQuestions.user_id == user.user_id).order_by(
-        LQuestions.question_id.desc()).first()
-    
-    if question is None:
-        return {'question_id': 1}
-    return {'question_id': question.question_id, 'content': question.content}
-
 class ContentData(BaseModel):
     content: str
 
@@ -45,17 +33,28 @@ async def submit_answer(id: int, content: ContentData, user=Depends(get_current_
     if question.user_id != user.user_id:
         raise HTTPException(status_code=403, detail="질문에 답변할 수 없습니다.")
     
+    user = db.query(LUsers).filter(LUsers.user_id == user.user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    # set user last question id
+    
     answer = LAnswers(user_id=user.user_id, question_id=id, content=content.content)
     db.add(answer)
     db.commit()
     db.refresh(answer)
 
     result = make_followed_question(id, db)
+
     if result['status'] == 'success':
+        user.last_answered_question_id = result['id']
+        db.commit()
         return {
             'question_id': result['id'],
         }
     else:
+        user.last_answered_question_id = question.next_question_id
+        db.commit()
         return {
             'question_id': question.next_question_id
         }
@@ -67,6 +66,24 @@ async def make_followed_question(parent_id: int, db: Session):
             'status': 'fail',
             'message': '최대 꼬리질문 개수를 초과했습니다.'
         }
+    context = []
+    p = parent
+    while p is not None:
+        context.append(
+            {
+                'speaker': 'user',
+                'text': db.query(LAnswers).filter(LAnswers.question_id == p.parents_id).filter(LAnswers.user_id == p.user_id).first().content
+            }
+        )
+        context.append(
+            {
+                'speaker': 'ai',
+                'text': p.content
+            }
+        )
+        p = db.query(LQuestions).filter(LQuestions.question_id == p.parents_id).first()
+    context.reverse()
+    print(context)
     content = "꼬리질문입니다" #여기에 ai로 새 꼬리질문 만들어 넣기
     followed = LQuestions(user_id=parent.user_id, parents_id=parent_id, content=content, is_answerable=True, level=parent.level+1)
     db.add(followed)
