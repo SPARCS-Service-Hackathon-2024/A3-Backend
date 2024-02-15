75c606c0c9bcebfe8c629437d5df1401e2b0bf48
r4bb1t <r4bb1t.github@gmail.com>
2024-02-15 20:22:21 +0900

    fix: 공통질문 버그

--- src/routers/question.py
+++ src/routers/question.py
@@ -22,7 +22,7 @@ async def get_question(id: int, db: Session = Depends(get_db)):
 class ContentData(BaseModel):
     content: str
 
-MAX_FOLLOWED_QUESTION = 5
+MAX_FOLLOWED_QUESTION = 4
 
 @router.post("/{id}",
              summary="질문에 답변하기")
@@ -30,7 +30,7 @@ async def submit_answer(id: int, content: ContentData, user=Depends(get_current_
     question = db.query(LQuestions).filter(LQuestions.question_id == id).first()
     if not question.is_answerable:
         raise HTTPException(status_code=400, detail="답변할 수 없는 질문입니다.")
-    if question.user_id != user.user_id:
+    if question.user_id != user.user_id and question.user_id != -1:
         raise HTTPException(status_code=403, detail="질문에 답변할 수 없습니다.")
     
     user = db.query(LUsers).filter(LUsers.user_id == user.user_id).first()
@@ -44,7 +44,7 @@ async def submit_answer(id: int, content: ContentData, user=Depends(get_current_
     db.commit()
     db.refresh(answer)
 
-    result = make_followed_question(id, db)
+    result = await make_followed_question(id, db)
 
     if result['status'] == 'success':
         user.last_answered_question_id = result['id']
@@ -71,6 +71,7 @@ async def make_followed_question(parent_id: int, db: Session):
     while p is not None:
         a = db.query(LAnswers).filter(LAnswers.question_id == p.parents_id).filter(LAnswers.user_id == p.user_id).first()
         if a is None:
+            p = db.query(LQuestions).filter(LQuestions.question_id == p.parents_id).first()
             continue
         context.append(
             {
@@ -88,7 +89,7 @@ async def make_followed_question(parent_id: int, db: Session):
     context.reverse()
     print(context)
     content = "꼬리질문입니다" #여기에 ai로 새 꼬리질문 만들어 넣기
-    followed = LQuestions(user_id=parent.user_id, parents_id=parent_id, content=content, is_answerable=True, level=parent.level+1)
+    followed = LQuestions(user_id=parent.user_id, parents_id=parent_id, content=content, is_answerable=True, level=parent.level+1, chapter_id=parent.chapter_id, next_question_id=parent.next_question_id)
     db.add(followed)
     db.commit()
     db.refresh(followed)
@@ -102,7 +103,7 @@ async def make_followed_question(parent_id: int, db: Session):
 async def skip_question(id: int, user=Depends(get_current_user), db: Session = Depends(get_db)):
     question = db.query(LQuestions).filter(LQuestions.question_id == id).first()
 
-    if question.user_id != user.user_id:
+    if question.user_id != user.user_id and question.user_id != -1:
         raise HTTPException(status_code=403, detail="질문을 스킵할 수 없습니다.")
     
     user = db.query(LUsers).filter(LUsers.user_id == user.user_id).first()
