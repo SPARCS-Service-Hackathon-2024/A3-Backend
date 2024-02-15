39f04460b79cce3ffa213a831a6399d959ad2496
mjkweon17 <mjkweon17@korea.ac.kr>
2024-02-15 16:23:13 +0900

    feat: auth/kakao param에서 body로 수정

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -5,12 +5,17 @@ from database import get_db
 import requests
 from models import LUsers
 
+from pydantic import BaseModel, Field
+
 router = APIRouter()
 
+class TokenData(BaseModel):
+    id_token: str
+
 @router.post("/kakao")
-async def kakao_login(id_token: str, db: Session = Depends(get_db)):
+async def kakao_login(token_data: TokenData, db: Session = Depends(get_db)):
     headers = {
-        "Authorization": f"Bearer {id_token}"
+        "Authorization": f"Bearer {token_data.id_token}"
     }
     response = requests.post("https://kapi.kakao.com/v2/user/me", headers=headers)
     if response.status_code == 200:
@@ -20,6 +25,8 @@ async def kakao_login(id_token: str, db: Session = Depends(get_db)):
         raise HTTPException(status_code=response.status_code, detail="Failed to fetch user information from Kakao")
     
     user = db.query(LUsers).filter(LUsers.kakao_id == user_info['id']).first()
+
+    # access token 만들기
 
     if user is None:
         new_user = LUsers(kakao_id=user_info['id'], name=user_info['properties']['nickname'])
