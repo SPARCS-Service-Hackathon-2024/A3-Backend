de475bbb7eb70ca45b1222bb3918b85920d04cd0
mjkweon17 <mjkweon17@korea.ac.kr>
2024-02-15 17:49:32 +0900

    chore: auth import문 앞에 routers 붙임

--- src/routers/question.py
+++ src/routers/question.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from database import get_db
 from models import LQuestions
 
-from auth import get_current_user
+from routers.auth import get_current_user
 
 router = APIRouter()
 
@@ -20,10 +20,11 @@ async def get_question(id: int, db: Session = Depends(get_db)):
 
 
 @router.get("/last",
-    summary="마지막으로 답변한 질문의 id 가져오기")
+            summary="마지막으로 답변한 질문의 id 가져오기")
 async def get_last_question(user=Depends(get_current_user), db: Session = Depends(get_db)):
     question = db.query(LQuestions).filter(LQuestions.user_id == user.user_id).order_by(
         LQuestions.question_id.desc()).first()
+    
     if question is None:
         return {'question_id': 1}
     return {'question_id': question.question_id, 'content': question.content}
